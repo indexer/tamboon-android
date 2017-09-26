4880e7c197e2a76b3a627106449bd367394fd508
yemonkyaw <yemonkyaw007@gmail.com>
2017-09-26 16:33:16 +0800

    update gitignore

--- app/src/main/java/com/indexer/tamboon/activities/DonateActivity.java
+++ app/src/main/java/com/indexer/tamboon/activities/DonateActivity.java
@@ -1,12 +1,11 @@
 package com.indexer.tamboon.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import butterknife.BindView;
@@ -14,8 +13,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import co.omise.android.models.Token;
 import co.omise.android.ui.CreditCardActivity;
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
 import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
 import com.github.javiersantos.materialstyleddialogs.enums.Style;
 import com.google.gson.JsonObject;
--- app/src/main/java/com/indexer/tamboon/viewmodel/CharitiesListViewModel.java
+++ app/src/main/java/com/indexer/tamboon/viewmodel/CharitiesListViewModel.java
@@ -16,13 +16,11 @@ import retrofit2.Response;
 public class CharitiesListViewModel extends AndroidViewModel {
 
   private MutableLiveData<List<Charity>> mCharitiesList;
-  private List<Charity> mListCharities = new ArrayList<>();
 
   public LiveData<List<Charity>> getCharities() {
     if (mCharitiesList == null) {
       mCharitiesList = new MutableLiveData<>();
       fetchData();
-      mCharitiesList.postValue(mListCharities);
     }
     return mCharitiesList;
   }
@@ -33,14 +31,13 @@ public class CharitiesListViewModel extends AndroidViewModel {
 
   private void fetchData() {
     Call<List<Charity>> productReturnObjectCall =
-        RestClient.getService(this.getApplication().getApplicationContext())
-            .getCharities();
+        RestClient.getService(this.getApplication().getApplicationContext()).getCharities();
     productReturnObjectCall.enqueue(new Callback<List<Charity>>() {
 
       @Override public void onResponse(@NonNull Call<List<Charity>> call,
           @NonNull Response<List<Charity>> response) {
         if (response.isSuccessful()) {
-          mListCharities = response.body();
+          mCharitiesList.setValue(response.body());
         }
       }
 
